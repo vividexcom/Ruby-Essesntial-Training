31f075f43565628fc4bff8982333cad0cceb640b
vividexcom <81256788+vividexcom@users.noreply.github.com>
2021-09-03 13:37:10 -0700

    learn hashes

--- Ruby_training/Ex_Files_Ruby_EssT_1_Basics/Exercise Files/Chapter_02/02_08/02_08_array_methods.rb
+++ Ruby_training/Ex_Files_Ruby_EssT_1_Basics/Exercise Files/Chapter_02/02_08/02_08_array_methods.rb
@@ -5,11 +5,11 @@
 # irb
 array = [2, 4, ['a', 'b'], nil, 4, 'c']
 # => [2, 4, ["a", "b"], nil, 4, "c"]
-array.length
+array.length      #check lendth of the array
 # => 6
-array.size
+array.size                         #check size of the array
 # => 6
-array.reverse
+array.reverse                           #reverse the array
 # => ["c", 4, nil, ["a", "b"], 4, 2]
 array
 # => [2, 4, ["a", "b"], nil, 4, "c"]
@@ -19,54 +19,59 @@ array
 # => ["c", 4, nil, ["a", "b"], 4, 2]
 array.shuffle
 # => [4, "c", 4, 2, nil, ["a", "b"]]
-array.shuffle
+array.shuffle                          #shuffle around items in the array
 # => [4, 2, ["a", "b"], 4, nil, "c"]
-array.shuffle!
+array.shuffle!                         #permanently shuffle around items in the array
 # => ["c", 4, ["a", "b"], nil, 2, 4]
-array.uniq
+array.uniq                         #removes duplicated items
 # => ["c", 4, ["a", "b"], nil, 2]
-array.uniq!
+array.uniq!                         #permanently removes duplicated items
 # => ["c", 4, ["a", "b"], nil, 2]
-array.compact
+array.compact                    #removes spacec (nil) inside the array
 # => ["c", 4, ["a", "b"], 2]
-array.compact!
+array.compact!                   # permantly removes spacec (nil) inside the array
 # => ["c", 4, ["a", "b"], 2]
-array.flatten
+array.flatten                     #make it a single array, remove array inside array
 # => ["c", 4, "a", "b", 2]
-array.flatten!
+array.flatten!                   #permanently make it a single array, remove array inside array
 # => ["c", 4, "a", "b", 2]
 array.include?(2)
 # => true
-array.include?(1)
+array.include?(1)   #search if 1 is included in the array
 # => false
-array.delete_at(1)
+array.delete_at(1)   #delete an item at the specified index 
 # => 4
 array
 # => ["c", "a", "b", 2]
-array.delete('c')
+array.delete('c')   #delete C in the array
 # => "c"
 array
 # => ["a", "b", 2]
-[1,2,3,4].join(',')
+[1,2,3,4].join(',') #join with comma's
 # => "1,2,3,4"
-[1,2,3,4].join
+[1,2,3,4].join  #join the array
 # => "1234"
-[1,2,3,4].join(' - ')
+[1,2,3,4].join(' - ')  #join the array with - in between
 # => "1 - 2 - 3 - 4"
 "1,2,3,4".split(',')
 # => ["1", "2", "3", "4"]
-numbers = [1,2,3,4]
+numbers = [1,2,3,4] #set array variable
 # => [1, 2, 3, 4]
-numbers.push(5)
+numbers.push(5)   #add 5 at the end of array (add value at the end of the array)
 # => [1, 2, 3, 4, 5]
-last = numbers.pop
+last = numbers.pop   #removes the last value in the array
 # = 5
 numbers
 # => [1, 2, 3, 4]
-first = numbers.shift
+first = numbers.shift  #removes first number in the array 
 # 1
 numbers
 # => [2, 3, 4]
-numbers.unshift(1)
+numbers.unshift(1)  #add 1 to the front of array
 # => [1, 2, 3, 4]
+#Adding arrays
+[1, 2, 3, 4] + [5, 6]
+# => [1, 2, 3, 4, 5, 6]
+
+
 quit
